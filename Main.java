ffef8c7e048f852dc2537d7432707a868b917db1
James Coates <jamescoates@zipcoders-MacBook-Pro.local>
2019-02-05 19:43:40 -0500

    Final game, too large too small

--- Main.java
+++ Main.java
@@ -5,6 +5,8 @@
  * @author (your name)
  * @version (a version number or a date)
  */
+import java.util.Scanner;
+import java.util.Random;
 public class Main
 {
     // instance variables - replace the example below with your own
@@ -15,8 +17,7 @@ public class Main
      */
     public Main()
     {
-        // initialise instance variables
-        x = 0;
+        game();
     }
 
     /**
@@ -25,9 +26,35 @@ public class Main
      * @param  y  a sample parameter for a method
      * @return    the sum of x and y
      */
-    public int sampleMethod(int y)
+    public static void game()
     {
-        // put your code here
-        return x + y;
-    }
+         boolean answer = false;
+         int count = 0;
+         int randomNumber = (int)(Math.random()*100 + 1);
+         int dummy = -1;
+         while(!answer) {
+             Scanner in = new Scanner(System.in);
+             System.out.println("\nEnter a number between 0 and 100, if you dare");
+             int guess = in.nextInt();
+             if(guess != dummy){
+                 count++;
+                }
+             dummy = guess; 
+             if(guess < 0 || guess > 100) {
+                 System.out.print("Please enter a number between 0 and 100 ");
+                }
+             else if(guess < randomNumber) {
+                 System.out.print("Your guess is to low ");
+                }
+             else if(guess == randomNumber) {
+                 System.out.print("Winner ");
+                 answer = true;
+                }
+             else if (guess > randomNumber) {
+                 System.out.print("Your guess is too high");
+                }
+             
+            }
+         System.out.print("congrats you guessed " + randomNumber + " in " + count + " guesses");            
+        }        
 }
